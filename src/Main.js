8590d5112d907dfe3a81a3034d301d1357e0b63c
a2c <atsushi1230.sdi@gmail.com>
2021-11-16 23:19:06 +0900

    :penguin: set colorme token  property

---
+++ src/Main.js
@@ -0,0 +1,4 @@
+const execute = () => {
+    const scriptProperties = PropertiesService.getScriptProperties();
+    const colormeAccessToken = ScriptProperties.getScriptProperties("COLORME_ACCESS_TOKEN")
+}
