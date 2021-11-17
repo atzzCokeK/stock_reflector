31efe9abee34dcbbba1079038ddf7ef79a0068b4
a2c <atsushi1230.sdi@gmail.com>
2021-11-18 00:24:03 +0900

    fix property name

--- src/Main.ts
+++ src/Main.ts
@@ -2,7 +2,7 @@
 const execute = () => {
     const scriptProperties: GoogleAppsScript.Properties.Properties = PropertiesService.getScriptProperties();
     const colormeAccessToken: string = scriptProperties.getProperty("COLORME_ACCESS_TOKEN")
-    const sheetId: string = scriptProperties.getProperty("COLORME_ACCESS_TOKEN")
+    const sheetId: string = scriptProperties.getProperty("STOCK_SHEET_ID")
 
     const stockSheet = new StockSheet(sheetId)
 }
