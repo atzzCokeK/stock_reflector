0bf8d0228550994c92ab6689388886fb2d052644
a2c <atsushi1230.sdi@gmail.com>
2021-12-04 16:26:04 +0900

    update Main.ts

--- src/Main.ts
+++ src/Main.ts
@@ -1,8 +1,13 @@
-
 const execute = () => {
-    const scriptProperties: GoogleAppsScript.Properties.Properties = PropertiesService.getScriptProperties();
-    const colormeAccessToken: string = scriptProperties.getProperty("COLORME_ACCESS_TOKEN")
-    const sheetId: string = scriptProperties.getProperty("STOCK_SHEET_ID")
+  const scriptProperties: GoogleAppsScript.Properties.Properties = PropertiesService.getScriptProperties()
+  const colorMeAccessToken: string | null = scriptProperties.getProperty('COLORME_ACCESS_TOKEN')
+  const sheetId: string | null = scriptProperties.getProperty('STOCK_SHEET_ID')
 
-    const stockSheet = new StockSheet(sheetId)
+  if (!colorMeAccessToken || !sheetId) {
+    throw new Error('ColorMeAccessTokenか、SheetIdを取得できていません。')
+  }
+
+  const gasService = new GASService(sheetId)
+  const stockSheet = gasService.getSheet()
+  const stockData = gasService.getStockRawData(stockSheet)
 }
